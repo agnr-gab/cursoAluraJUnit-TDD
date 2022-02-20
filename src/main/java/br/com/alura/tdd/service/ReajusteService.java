5da2b4246861baeae7704f8acf42ec0ac594f68c
Arê Nascimento <aretuza.gab@gmail.com>
2022-02-19 21:51:50 -0300

    Criar condição para reajuste otimo

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -6,13 +6,19 @@ import br.com.alura.tdd.modelo.Funcionario;
 import java.math.BigDecimal;
 
 public class ReajusteService {
+
     public void concenderReajuste(Funcionario funcionario, Desempenho desempenho) {
+
         if (desempenho == Desempenho.A_DESEJAR) {
             BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
             funcionario.reajustarSalario(reajuste);
         }
         if (desempenho == Desempenho.BOM) {
             BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
+            funcionario.reajustarSalario(reajuste);
+        }
+        if (desempenho == Desempenho.OTIMO) {
+            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.30"));
             funcionario.reajustarSalario(reajuste);
         }
     }
